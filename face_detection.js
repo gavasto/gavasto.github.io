407a4147be8c0a2a906132c172506d198ab36665
gavasto <81271153+gavasto@users.noreply.github.com>
2023-04-05 10:58:58 +0700

    html and face detect js

---
+++ face_detection.js
@@ -0,0 +1,26 @@
+import * as faceapi from 'face-api.js';
+
+$(document).ready(function(){    
+    async function face(){
+        
+        const MODEL_URL = '/weights'
+
+        await faceapi.loadSsdMobilenetv1Model(MODEL_URL)
+        await faceapi.loadFaceLandmarkModel(MODEL_URL)
+        await faceapi.loadFaceRecognitionModel(MODEL_URL)
+        await faceapi.loadFaceExpressionModel(MODEL_URL)
+
+        const video= document.getElementById('demo')
+        let faceDescriptions = await faceapi.detectAllFaces(video)
+        const canvas = $('#reflay').get(0)
+        faceapi.matchDimensions(canvas, video)
+
+        faceDescriptions = faceapi.resizeResults(faceDescriptions, video)
+        faceapi.draw.drawDetections(canvas, faceDescriptions)
+        faceapi.draw.drawFaceLandmarks(canvas, faceDescriptions)
+        faceapi.draw.drawFaceExpressions(canvas, faceDescriptions)
+
+    }
+    
+    face()
+})
