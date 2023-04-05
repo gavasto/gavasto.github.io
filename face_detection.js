0c10b5dafd8d6f4201d9a405d3f17708ca866295
gavasto <81271153+gavasto@users.noreply.github.com>
2023-04-05 22:12:33 +0700

    Update face_detection.js

--- face_detection.js
+++ face_detection.js
@@ -4,7 +4,7 @@
         
         const MODEL_URL = '/weights'
 
-        Promise(faceapi.loadTinyFaceDetectorModel(MODEL_URL))
+        Promise(faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL))
         //await faceapi.loadFaceLandmarkModel(MODEL_URL)
         //await faceapi.loadFaceRecognitionModel(MODEL_URL)
         //await faceapi.loadFaceExpressionModel(MODEL_URL)
@@ -16,12 +16,13 @@
             let container = document.querySelector(".container");
             container.append(canvas);
             //const canvas = document.getElementById("myCanvas")
-            faceapi.matchDimensions(canvas, video)
+            const displaySize = { width: video.width, height: video.height };
+            faceapi.matchDimensions(canvas, displaySize)
             setInterval(async () => {
                 let faceDescriptions = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions());
-                //canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+                canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
     
-                faceDescriptions = faceapi.resizeResults(faceDescriptions, video)
+                faceDescriptions = faceapi.resizeResults(faceDescriptions, displaySize)
                 faceapi.draw.drawDetections(canvas, faceDescriptions)
                 //faceapi.draw.drawFaceLandmarks(canvas, faceDescriptions)
                 //faceapi.draw.drawFaceExpressions(canvas, faceDescriptions)
