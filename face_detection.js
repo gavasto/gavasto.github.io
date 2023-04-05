5a41c293c30e167e9d4787a66435c0059a35f9dc
gavasto <81271153+gavasto@users.noreply.github.com>
2023-04-05 17:07:35 +0700

    Update face_detection.js

--- face_detection.js
+++ face_detection.js
@@ -12,7 +12,7 @@ import * as faceapi from 'face-api.js';
         const video= document.getElementById('demo')
         video.onplay = function() {
             //const canvas = faceapi.createCanvasFromMedia(video);
-            const canvas = faceapi.createCanvas(640, 360);
+            const canvas = faceapi.createCanvas({width: 640, height: 360});
             //const canvas = document.getElementById("myCanvas")
             setInterval(async () => {
                 let faceDescriptions = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions());
