2367a27b72e91347c2785c537c059dd33ca708a4
gavasto <81271153+gavasto@users.noreply.github.com>
2023-04-05 14:02:15 +0700

    update

--- face_detection.js
+++ face_detection.js
@@ -1,27 +1,29 @@
 import * as faceapi from 'face-api.js';
 
-$(document).ready(function(){    
+    
     async function face(){
         
         const MODEL_URL = '/weights'
 
         await faceapi.loadTinyFaceDetectorModel(MODEL_URL)
-        await faceapi.loadFaceLandmarkModel(MODEL_URL)
-        await faceapi.loadFaceRecognitionModel(MODEL_URL)
-        await faceapi.loadFaceExpressionModel(MODEL_URL)
+        //await faceapi.loadFaceLandmarkModel(MODEL_URL)
+        //await faceapi.loadFaceRecognitionModel(MODEL_URL)
+        //await faceapi.loadFaceExpressionModel(MODEL_URL)
 
-        const video = document.getElementById('originalImg')
-        //setInterval(async () => {
-            let faceDescriptions = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
-            const canvas = document.getElementById("myCanvas")
-            var context = canvas.getContext("2d")
-            faceapi.matchDimensions(canvas, video)
-
-            faceDescriptions = faceapi.resizeResults(faceDescriptions, video)
-            faceapi.draw.drawDetections(canvas, faceDescriptions)
-            faceapi.draw.drawFaceLandmarks(canvas, faceDescriptions)
-            faceapi.draw.drawFaceExpressions(canvas, faceDescriptions)
-        //}, 100);
+        const video = document.getElementById('demo')
+        video.onplay = function() {
+            const canvas = faceapi.createCanvasFromMedia(video);
+            //const canvas = document.getElementById("myCanvas")
+            setInterval(async () => {
+                let faceDescriptions = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions());
+                //var context = canvas.getContext("2d")
+                faceapi.matchDimensions(canvas, video)
+    
+                faceDescriptions = faceapi.resizeResults(faceDescriptions, video)
+                faceapi.draw.drawDetections(canvas, faceDescriptions)
+                //faceapi.draw.drawFaceLandmarks(canvas, faceDescriptions)
+                //faceapi.draw.drawFaceExpressions(canvas, faceDescriptions)
+            }, 100);
+        }
     }
     face()
-})
